c28ba86953f61d27cc5481d48eb68789bd5108de
Ahmed <zaoui.ahmed.work@gmail.com>
2025-12-06 16:05:32 +0100

    formatted the file for pep8 convention

--- RandomForest.py
+++ RandomForest.py
@@ -43,7 +43,8 @@ for col in cat_cols:
 X = df.drop('Survived', axis=1)
 y = df['Survived']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
 
 # -------------------------
 # Train Random Forest
